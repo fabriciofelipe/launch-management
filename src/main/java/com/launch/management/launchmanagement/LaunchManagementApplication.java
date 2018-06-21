7db7814757714eb0acf9b385d0cff2bea40ed8aa
fabriciofelipe <portalf@gmail.com>
2018-06-21 17:16:25 -0300

    first commit

---
+++ src/main/java/com/launch/management/launchmanagement/LaunchManagementApplication.java
@@ -0,0 +1,12 @@
+package com.launch.management.launchmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchManagementApplication.class, args);
+	}
+}
